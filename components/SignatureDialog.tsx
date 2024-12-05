b642bcfebae619f85e38bf199e6fbedc877de56e
franklin ramos <ramosfp99@gmail.com>
2024-12-05 13:29:48 +0800

    uploading svg logo

--- components/SignatureDialog.tsx
+++ components/SignatureDialog.tsx
@@ -8,10 +8,14 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Image from "next/image";
 import Globe from "@/public/globe.svg";
+import AOG from "@/public/AOG-logo.svg";
+import AOW from "@/public/AOW-logo.svg";
+import AOH from "@/public/AOH-logo.svg";
 import Phone from "@/public/phone-call.svg";
+import { Copy } from "lucide-react";
 
 interface SignatureDialogProps {
   name: string;
@@ -21,6 +25,20 @@ interface SignatureDialogProps {
 
 const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
   const [openDialog, setOpenDialog] = useState(false);
+
+  const handleCopy = () => {
+    if (signatureRef.current) {
+      navigator.clipboard
+        .writeText(signatureRef.current.outerHTML)
+        .then(() => console.log("Copied successfully"))
+        .catch((error) => console.error(error));
+    } else {
+      console.error("Signature element not found.");
+    }
+  };
+
+  const signatureRef = useRef<HTMLDivElement | null>(null);
+
   return (
     <>
       <Button
@@ -31,7 +49,13 @@ const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
         Show signature
       </Button>
       <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-        <DialogContent className="w-full">
+        <DialogContent
+          className="w-full"
+          style={{
+            maxWidth: "750px", // Set the dialog's width to 800px
+            width: "100%", // Ensure it is responsive
+          }}
+        >
           <DialogHeader>
             <DialogTitle>Copy your signature base on usage.</DialogTitle>
             <DialogDescription>Signature details</DialogDescription>
@@ -42,6 +66,7 @@ const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
               width: "700px",
               borderRadius: "12px",
             }}
+            ref={signatureRef}
           >
             <div
               style={{
@@ -56,7 +81,7 @@ const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
                 style={{ display: "flex", gap: "12px", alignItems: "center" }}
               >
                 <Image
-                  src="https://adongroup.com.au/wp-content/uploads/2024/12/AdonGroup.png"
+                  src={AOG}
                   alt="Logo"
                   width={190}
                   height={120}
@@ -147,7 +172,7 @@ const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
                   }}
                 >
                   <Image
-                    src="https://adonworkforce.com.au/wp-content/uploads/2023/03/Ad-on-Workforce-logo.png"
+                    src={AOW}
                     alt="AOW logo"
                     width={200}
                     height={80}
@@ -155,7 +180,7 @@ const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
                     priority
                   />
                   <Image
-                    src="https://adongroup.com.au/wp-content/uploads/2024/12/AOH-logo.png"
+                    src={AOH}
                     alt="AOH logo"
                     width={120}
                     height={80}
@@ -177,6 +202,24 @@ const SignatureDialog = ({ name, position, contact }: SignatureDialogProps) => {
                 its return.
               </span>
             </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <Button
+              variant={"default"}
+              size={"sm"}
+              className="mt-4"
+              onClick={handleCopy} // Use the ref-based handler
+            >
+              <Copy /> Copy signature
+            </Button>
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="mt-4"
+              onClick={handleCopy} // Use the ref-based handler
+            >
+              <Copy /> Copy signature for Monday
+            </Button>
           </div>
         </DialogContent>
       </Dialog>
